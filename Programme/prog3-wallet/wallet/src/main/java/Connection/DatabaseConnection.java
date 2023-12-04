0f1164d097ceca428d2f92e0fa96c183776bae13
= <=>
2023-12-04 18:31:32 +0300

    feat: add connection using jdbc postgres

---
+++ Programme/prog3-wallet/wallet/src/main/java/Connection/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package Connection;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.lang.Class.forName;
+
+public class DatabaseConnection {
+    @Value("${spring.datasource.url")
+    private String url;
+    @Value("${spring.datasource.user}")
+    private String user;
+    @Value("${sring.datasource.password")
+    private String password;
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+}
