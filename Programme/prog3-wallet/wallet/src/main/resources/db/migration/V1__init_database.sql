7f98874f0be3ab6c1d4795f187c1dff17f399e58
= <=>
2023-12-08 16:03:00 +0300

    fix conflit delete migration script

--- Programme/prog3-wallet/wallet/src/main/resources/db/migration/V1__init_database.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists user (
-    id_user int serial primary key,
-    user_name varchar(150) not null,
-    type_compte varchar(20) check(type in ('Especes', 'Carte de credit')) not null
-    devise varchar(3) check(type in ('MGA', 'EUR', 'USD', 'GBP')) not null,
-    montant_de_depart float;
-);
