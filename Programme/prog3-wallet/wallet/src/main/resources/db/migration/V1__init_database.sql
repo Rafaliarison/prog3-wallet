2f7967e1125df3fbf8890429efb877d6645d4457
= <=>
2023-12-08 16:28:43 +0300

    fix: indentation migration

--- Programme/prog3-wallet/wallet/src/main/resources/db/migration/V1__init_database.sql
+++ Programme/prog3-wallet/wallet/src/main/resources/db/migration/V1__init_database.sql
@@ -1,20 +1,20 @@
 create table if not exists account (
-                                       account_id serial primary key ,
-                                       account_name varchar(100) check ( account_name in ('current_account', 'save_account')),
-                                       account_solde float,
-                                       account_update_date timestamp with time zone,
-                                       account_curracy int
+    account_id serial primary key ,
+    account_name varchar(100) check ( account_name in ('current_account', 'save_account')),
+    account_solde float,
+    account_update_date timestamp with time zone,
+    account_curracy int
 );
 
 create table if not exists curracy (
-                                       curracy_id serial primary key ,
-                                       curracy_name varchar(50),
-                                       curracy_code varchar(3) check ( curracy_code in ('EUR', 'MGA'))
+    curracy_id serial primary key ,
+    curracy_name varchar(50),
+    curracy_code varchar(3) check ( curracy_code in ('EUR', 'MGA'))
 );
 
 create table if not exists transaction (
-                                           transaction_id serial primary key ,
-                                           transaction_label varchar(100),
-                                           transaction_date timestamp with time zone,
-                                           transaction_type varchar(10) check ( transaction_type in ('debit', 'credit'))
+    transaction_id serial primary key ,
+    transaction_label varchar(100),
+    transaction_date timestamp with time zone,
+    transaction_type varchar(10) check ( transaction_type in ('debit', 'credit'))
 );
